4ba77916e679e1fb0799fe5023e3adefbdb73488
AdamHoppe98 <adamhoppe98@gmail.com>
2025-10-21 10:59:37 +0200

    refactor wishrepo

--- src/main/java/com/example/wishlist/Model/User.java
+++ src/main/java/com/example/wishlist/Model/User.java
@@ -1,4 +1,49 @@
 package com.example.wishlist.Model;
 
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String Password;
+
+    public User(int id, String password, String email, String name) {
+        this.id = id;
+        Password = password;
+        this.email = email;
+        this.name = name;
+    }
+
+    public User(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
