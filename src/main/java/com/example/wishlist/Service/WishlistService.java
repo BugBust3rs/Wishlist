bfc688bfd86a8e21304b1015cac81fbe2e98b272
Silverspit <philipraissnia-2@hotmail.com>
2025-10-21 12:59:46 +0200

    wish model

--- src/main/java/com/example/wishlist/Service/WishlistService.java
+++ src/main/java/com/example/wishlist/Service/WishlistService.java
@@ -1,7 +1,21 @@
 package com.example.wishlist.Service;
 
+import com.example.wishlist.Model.User;
+import com.example.wishlist.Repository.WishListRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishlistService {
+    private final WishListRepository wishListRepository;
+
+
+    public WishlistService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+
+    public List<User> getUsers(){
+        return wishListRepository.getUsers();
+    }
 }
