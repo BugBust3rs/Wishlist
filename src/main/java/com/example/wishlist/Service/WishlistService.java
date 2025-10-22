3a89494f0fdfdb9b51063115528f76f197e42ae5
AdamHoppe98 <adamhoppe98@gmail.com>
2025-10-22 10:32:40 +0200

    service small thing

--- src/main/java/com/example/wishlist/Service/WishlistService.java
+++ src/main/java/com/example/wishlist/Service/WishlistService.java
@@ -1,13 +1,18 @@
 package com.example.wishlist.Service;
 
 
+import com.example.wishlist.Repository.WishListRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class WishlistService {
 
+    private final WishListRepository repository;
+
+    public WishlistService(WishListRepository repository){
+        this.repository = repository;
+    }
 
 
 }
