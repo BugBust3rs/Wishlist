39c6827e49344e6b707defd6a496b963c4ccbcde
AdamHoppe98 <adamhoppe98@gmail.com>
2025-10-22 09:47:07 +0200

    injection

--- src/main/java/com/example/wishlist/Controller/WishlistController.java
+++ src/main/java/com/example/wishlist/Controller/WishlistController.java
@@ -1,7 +1,16 @@
 package com.example.wishlist.Controller;
 
+import com.example.wishlist.Service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("")
 @Controller
 public class WishlistController {
+
+    private final WishlistService service;
+
+    public WishlistController(WishlistService service){
+        this.service = service;
+    }
 }
