cc8ec5ab95c776d6009c4dd0cfd418d804584e43
josefinehoerlin <josefinehoerlin@gmail.com>
2025-10-27 14:04:23 +0100

    first commit

--- src/main/java/com/example/wishlist/Controller/WishlistController.java
+++ src/main/java/com/example/wishlist/Controller/WishlistController.java
@@ -1,9 +1,11 @@
 package com.example.wishlist.Controller;
 
+import com.example.wishlist.Model.User;
 import com.example.wishlist.Service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -20,5 +22,12 @@ public class WishlistController {
     @GetMapping
     public String index() {
         return "wishlist";
+    }
+
+    @GetMapping("/createUser")
+    public String showCreateUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "createUser";
     }
 }
