b3f3594614d7fadae39cd60aed3c574c44061cea
Niinyah <petermarbell@gmail.com>
2025-11-03 09:02:28 +0100

    Updatewish

--- Docs/SQLTableCreator.sql
+++ Docs/SQLTableCreator.sql
@@ -1,30 +1,35 @@
-DROP DATABASE IF EXISTS WishList;
-CREATE DATABASE WishList
+DROP
+DATABASE IF EXISTS WishList;
+CREATE
+DATABASE WishList
 DEFAULT CHARACTER SET utf8mb4;
-USE Wishlist;
+USE
+Wishlist;
 
-CREATE TABLE User (
-                      user_id INT AUTO_INCREMENT PRIMARY KEY,
-                      name VARCHAR(50) NOT NULL,
-                      password VARCHAR(50) NOT NULL,
-                      email VARCHAR(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE Wish (
-                      wish_id INT AUTO_INCREMENT PRIMARY KEY,
-                      wishlist_id      INT NOT NULL,
-                      name VARCHAR(150) NOT NULL,
-                      description VARCHAR(150),
-                      link VARCHAR(150),
-                      price INTEGER,
-                      isReserved BOOLEAN,
-                      FOREIGN KEY (wishlist_id) REFERENCES Wishlists(wishlist_id) ON DELETE CASCADE
+CREATE TABLE User
+(
+    user_id  INT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email    VARCHAR(50) NOT NULL UNIQUE
 );
 
 Create TABLE Wishlists
 (
-                      wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
-                      name VARCHAR(150) NOT NULL,
-                      user_id INT NOT NULL,
-                      FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+    wishlist_id INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(150) NOT NULL,
+    user_id     INT          NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Wish
+(
+    wish_id     INT AUTO_INCREMENT PRIMARY KEY,
+    wishlist_id INT          NOT NULL,
+    name        VARCHAR(150) NOT NULL,
+    description VARCHAR(150),
+    link        VARCHAR(150),
+    price       INTEGER,
+    isReserved  BOOLEAN,
+    FOREIGN KEY (wishlist_id) REFERENCES Wishlists (wishlist_id) ON DELETE CASCADE
 );
--- Docs/SQLTablePopulate.sql
+++ Docs/SQLTablePopulate.sql
@@ -1,7 +1,7 @@
 INSERT INTO `User` (name, password, email) VALUES
     ('Jeppe K', 'Jeppe123', 'Nytnytnyt@pagne.dk');
 
-INSERT INTO `Wish` (user_id, name, description, link, price, isReserved) VALUES
+INSERT INTO `Wishlists` (user_id, name, description, link, price, isReserved) VALUES
     (1, 'BMW M5', 'Ny dyt til far', 'https://www.bmw.dk/', 1000000, FALSE);
 
 INSERT INTO `Wish` (user_id, name, description, link, price, isReserved) VALUES
